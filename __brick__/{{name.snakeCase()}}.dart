2a724052acef99cdd03addeae62508bd1a1fac2d
kungpotato <kung.kittisak@bitkub.com>
2024-08-15 12:36:01 +0700

    ok

--- __brick__/{{name.snakeCase()}}.dart
+++ __brick__/{{name.snakeCase()}}.dart
@@ -1,16 +1,16 @@
-import 'package:core_domain/{{name.snakeCase()}}/entities/{{feature.snakeCase()}}.dart';
-import 'package:core_domain/{{name.snakeCase()}}/usecases/{{feature.snakeCase()}}_params.dart';
+import 'package:core_domain/{{feature.snakeCase()}}/entities/{{name.snakeCase()}}.dart';
+import 'package:core_domain/{{feature.snakeCase()}}/usecases/{{name.snakeCase()}}_params.dart';
 import 'package:core_domain/src/success/success.dart';
 import 'package:core_domain/usecase.dart';
 
 ///
-/// extends NoParamsUseCase<Success<{{feature.pascalCase()}}Entity>> if no params
+/// extends NoParamsUseCase<Success<{{name.pascalCase()}}Entity>> if no params
 ///
 class {{feature.pascalCase()}}
-    extends UseCase<{{name.pascalCase()}}Params, Success<{{feature.pascalCase()}}Entity>> {
+    extends UseCase<{{name.pascalCase()}}Params, Success<{{name.pascalCase()}}Entity>> {
   {{name.pascalCase()}}(this.repository);
 
-  final {{name.pascalCase()}}Repository repository;
+  final {{feature.pascalCase()}}Repository repository;
 
   @override
   Stream<Success<{{feature.pascalCase()}}Entity>> call(
